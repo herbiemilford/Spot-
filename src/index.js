d61a365545b3fe9252372b20ca2bf3e17696dcf1
Joe Crowder <toast.one.art@gmail.com>
2019-03-09 20:38:23 -0600

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,60 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './index.html';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const reactFormContainer =
+document.querySelector('.react-form-container')
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class ReactFormLabel extends React.Component {
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return(
+            <label htmlFor={this.props.htmlFor}>
+            {this.props.title}
+            </label>
+        )
+    }
+}
+class ReactForm extends React.Component {
+    constructor(props){
+        super(props)
+
+        this.state ={
+            name: '',
+            email: '',
+            subject: '',
+            message:''
+        }
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+    handleChange =(e) => {
+        let newState = {}
+
+        newState[e.target.name] = e.target.value
+
+        this.setState(newState)
+    }
+
+    handleSubmit = (e, message) => {
+        e.preventDefault()
+
+        let formData = {
+            formSender: this.state.name,
+            formEmail: this.state.email,
+            formSubject: this.state.subject,
+            formMessage: this.state.message
+        }
+        if(formData.formSender.length < 1 ||
+            formData.formEmail.length < 1 ||
+            formData.formSubject.length < 1 ||
+            formData.formMessage.length < 1 ){
+                return false
+            }
+            
+    }
+}
+ReactDOM.render(<ReactForm />, reactFormContainer);
